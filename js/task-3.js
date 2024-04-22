e527cb06846430f4d75dfedb3eb97ad3c273b52c
Arsenkats <kats45mam@gmail.com>
2024-04-22 12:29:27 +0300

    complete task 1-4

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,20 @@
-function getElementWidth(content, padding, border) {
-    const totalPadding = Number.parseFloat(padding) * 2;
-    const totalBorder = Number.parseFloat(border) * 2;
-    const totalElementWidth = Number.parseFloat(content) + totalPadding + totalBorder;
-    return totalElementWidth;
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    const hasSpam = lowerCaseMessage.includes('spam');
+    const hasSale = lowerCaseMessage.includes('sale');
+
+    if (hasSpam || hasSale) {
+        return true;
+    } else {
+        return false;
+    }
+    
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
